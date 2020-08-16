dd12995db130f14f3472492ce1035f14a3fc5946
ToastHawaii <mar.pel@protonmail.com>
2020-08-16 15:29:17 +0200

    better display, better transfrom,

--- src/script.ts
+++ src/script.ts
@@ -48,13 +48,13 @@ function processNameWebsiteWiki(value: string = "") {
 }
 
 (async function () {
-  const softwareRequest = requestTemplates("Software", source => {
+  const softwareRequest = requestTemplates("Software", (source, wiki) => {
     const obj: {
       name: string;
       description: string;
       image?: string;
       website?: string;
-      wiki?: string;
+      wiki: string;
       languages: string[];
       themes: string[];
     } = {
@@ -62,75 +62,92 @@ function processNameWebsiteWiki(value: string = "") {
       description: source["description"] || "",
       image: toWikimediaUrl(source["screenshot"]),
       website: toUrl(source["web"]),
-      wiki: toWikiUrl(source["wiki"]),
-      languages: [],
-      themes: []
+      wiki: toWikiUrl(source["wiki"] || wiki) || "",
+      languages: (source["languages"] || "").split(/;/).filter(v => v),
+      themes: (source["genre"] || "")
+        .split(";")
+        .filter(v => v)
+        .map(v => `${v[0].toUpperCase()}${v.slice(1)}`)
     };
 
     let name = processNameWebsiteWiki(source["name"]);
     obj.name = name.name || obj.name;
     obj.website = obj.website || name.website;
-    obj.wiki = obj.wiki || name.wiki;
+    obj.wiki = obj.wiki || name.wiki || "";
 
     name = processNameWebsiteWiki(source["web"]);
     obj.name = obj.name || name.name;
     obj.website = name.website || obj.website;
-    obj.wiki = obj.wiki || name.wiki;
+    obj.wiki = obj.wiki || name.wiki || "";
 
     name = processNameWebsiteWiki(source["wiki"]);
     obj.name = obj.name || name.name;
     obj.website = obj.website || name.website;
     obj.wiki = name.wiki || obj.wiki;
 
-    const element = createElement(
-      "div",
-      `<div class="header">
-        <div class="name">${obj.name}</div>
-        ${
-          obj.image
-            ? `<img class="img" src="${obj.image}"/>`
-            : `<img class="img" src="https://wiki.openstreetmap.org/w/images/thumb/c/ca/Map-14.svg/140px-Map-14.svg.png"/>`
-        }
-      </div>
-      <div class="description">${obj.description}</div>
-       ${
-         obj.website
-           ? `<a target="_blank" href="${obj.website}">Website</a>`
-           : ""
-       }
-      ${obj.wiki ? `<a target="_blank" href="${obj.wiki}">Wiki</a>` : ""}`,
-      ["app"]
-    );
-
-    document.body.appendChild(element);
+    render(obj);
 
     console.info(obj);
   });
-  const serviceItemRequest = requestTemplates("Service item", source => {
-    const obj: {
-      name: string;
-      description: string;
-      image?: string;
-      website?: string;
-      wiki?: string;
-      languages: string[];
-      themes: string[];
-    } = {
-      name: source["name"] || "",
-      description: source["descr"] || "",
-      image: toWikimediaUrl(source["image"]),
-      languages: [],
-      themes: []
-    };
+  const serviceItemRequest = requestTemplates(
+    "Service item",
+    (source, wiki) => {
+      const obj: {
+        name: string;
+        description: string;
+        image?: string;
+        website?: string;
+        wiki: string;
+        languages: string[];
+        themes: string[];
+      } = {
+        name: source["name"] || "",
+        description: source["descr"] || "",
+        image: toWikimediaUrl(source["image"]),
+        wiki: toWikiUrl(wiki) || "",
+        languages: (source["lang"] || "")
+          .split(/,+(?![^\(]*\))/)
+          .map(v => {
+            const match = /:(\w{2})/.exec(v);
 
-    let name = processNameWebsiteWiki(source["name"]);
-    obj.name = name.name || obj.name;
-    obj.website = name.website;
-    obj.wiki = name.wiki;
+            if (match) return match[1];
+            return v;
+          })
+          .filter(v => v),
+        themes: (source["genre"] || "")
+          .split(/,+(?![^\(]*\))/)
+          .map(v => v.replace(/^[\.\s]+|[\.\s]+$/gm, ""))
+          .filter(v => v)
+          .map(v => `${v[0].toUpperCase()}${v.slice(1)}`)
+      };
 
-    const element = createElement(
-      "div",
-      `<div class="header">
+      let name = processNameWebsiteWiki(source["name"]);
+      obj.name = name.name || obj.name;
+      obj.website = name.website;
+      obj.wiki = obj.wiki || name.wiki || "";
+
+      render(obj);
+
+      console.info(obj);
+    }
+  );
+
+  await softwareRequest;
+  await serviceItemRequest;
+})();
+
+function render(obj: {
+  name: string;
+  description: string;
+  image?: string | undefined;
+  website?: string | undefined;
+  wiki?: string | undefined;
+  languages: string[];
+  themes: string[];
+}) {
+  const element = createElement(
+    "div",
+    `<div class="header">
         <div class="name">${obj.name}</div>
         ${
           obj.image
@@ -144,22 +161,44 @@ function processNameWebsiteWiki(value: string = "") {
           ? `<a target="_blank" href="${obj.website}">Website</a>`
           : ""
       }
-      ${obj.wiki ? `<a target="_blank" href="${obj.wiki}">Wiki</a>` : ""}`,
-      ["app"]
-    );
+      ${obj.wiki ? `<a target="_blank" href="${obj.wiki}">Wiki</a>` : ""}
+      <div class="themes">${obj.themes
+        .map(t => {
+          const background = textToColor(t);
 
-    document.body.appendChild(element);
+          const yiq =
+            (background.r * 299 + background.g * 587 + background.b * 114) /
+            1000;
 
-    console.info(obj);
-  });
+          return `<span class="thema" style="background: rgb(${background.r},${
+            background.g
+          },${background.b}); color:${
+            yiq >= 128 ? "black" : "white"
+          };">${t}</span>`;
+        })
+        .join("")}</div>`,
+    ["app"]
+  );
 
-  await softwareRequest;
-  await serviceItemRequest;
-})();
+  document.body.appendChild(element);
+}
+
+function textToColor(s: string) {
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (i % 3 === 0) r = (r + s.charCodeAt(i)) % 256;
+    else if (i % 3 === 1) g = (g + s.charCodeAt(i)) % 256;
+    else b = (b + s.charCodeAt(i)) % 256;
+  }
+  return { r, g, b };
+}
 
 function parseTemplateToObject(
+  title: string,
   content: string,
-  transform: (obj: { [name: string]: string }) => void
+  transform: (obj: { [name: string]: string }, title: string) => void
 ) {
   const obj: { [name: string]: string } = {};
   const props = content.split(/\|(?![^{]*})(?![^\[]*\])/g);
@@ -174,15 +213,19 @@ function parseTemplateToObject(
     if (value) obj[name] = value;
   }
 
-  transform(obj);
+  transform(obj, title);
 }
 
 function parsePage(
+  title: string,
   content: string,
   template: string,
-  transform: (obj: { [name: string]: string }) => void
+  transform: (obj: { [name: string]: string }, title: string) => void
 ) {
-  const start = content.indexOf("{{" + template);
+  //console.info(content);
+  const start = content.search(
+    new RegExp("{{" + template.replace(" ", "[_ ]"), "gi")
+  );
 
   if (start === -1) return;
 
@@ -198,15 +241,15 @@ function parsePage(
     .trim();
 
   console.info(templateContent);
-  parseTemplateToObject(templateContent, transform);
+  parseTemplateToObject(title, templateContent, transform);
 
-  parsePage(rest, template, transform);
+  parsePage(title, rest, template, transform);
 }
 
 async function loadPages(
   ids: string[],
   template: string,
-  transform: (obj: { [name: string]: string }) => void
+  transform: (obj: { [name: string]: string }, title: string) => void
 ) {
   const params: { [name: string]: string } = {
     prop: "revisions",
@@ -220,8 +263,7 @@ async function loadPages(
   const pages = response.query.pages;
   for (const p in pages) {
     const content = pages[p].revisions[0].slots.main.content;
-
-    parsePage(content, template, transform);
+    parsePage(pages[p].title, content, template, transform);
   }
 }
 
@@ -238,7 +280,7 @@ async function osmMediaApiQuery(params: { [name: string]: string }) {
 
 async function requestTemplates(
   template: string,
-  transform: (obj: { [name: string]: string }) => void
+  transform: (obj: { [name: string]: string }, title: string) => void
 ) {
   const params = {
     list: "embeddedin",
@@ -257,7 +299,7 @@ async function requestTemplates(
 function processPagesByTemplateResult(
   response: { continue: { eicontinue: any }; query: { embeddedin: any } },
   template: string,
-  transform: (obj: { [name: string]: string }) => void
+  transform: (obj: { [name: string]: string }, title: string) => void
 ) {
   const pages = response.query.embeddedin;
   let ids = [];
@@ -278,7 +320,7 @@ function processPagesByTemplateResult(
 async function loadPagesByTemplatePage(
   con: string,
   template: string,
-  transform: (obj: { [name: string]: string }) => void
+  transform: (obj: { [name: string]: string }, title: string) => void
 ) {
   const params = {
     list: "embeddedin",
