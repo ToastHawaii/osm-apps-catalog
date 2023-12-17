e0c39dfa5afcd0658e02df60a5758d74381a65e2
ToastHawaii <mar.pel@protonmail.com>
2023-12-17 19:25:29 +0100

    fix

--- src/data/i18n.ts
+++ src/data/i18n.ts
@@ -9,7 +9,7 @@ import * as ja from "./locales/ja.json";
 import * as nb_NO from "./locales/nb_NO.json";
 import * as pl from "./locales/pl.json";
 import * as ru from "./locales/ru.json";
-import * as ku from "./locales/ku.json";
+import * as uk from "./locales/uk.json";
 
 i18next.init({
   lng:
