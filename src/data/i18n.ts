7e6100997683a04cc551ac924e47a167044ac8ba
ToastHawaii <mar.pel@protonmail.com>
2023-12-17 18:09:17 +0100

    Add more languages

--- src/data/i18n.ts
+++ src/data/i18n.ts
@@ -1,7 +1,15 @@
 import i18next from "i18next";
 import * as en from "./locales/en.json";
+import * as cs from "./locales/cs.json";
 import * as de from "./locales/de.json";
+import * as es from "./locales/es.json";
+import * as fr from "./locales/fr.json";
+import * as it from "./locales/it.json";
+import * as ja from "./locales/ja.json";
 import * as nb_NO from "./locales/nb_NO.json";
+import * as pl from "./locales/pl.json";
+import * as ru from "./locales/ru.json";
+import * as ku from "./locales/ku.json";
 
 i18next.init({
   lng:
@@ -12,11 +20,35 @@ i18next.init({
     en: {
       translation: en,
     },
+    cs: {
+      translation: cs,
+    },
     de: {
       translation: de,
     },
+    es: {
+      translation: es,
+    },
+    fr: {
+      translation: fr,
+    },
+    it: {
+      translation: it,
+    },
+    ja: {
+      translation: ja,
+    },
     no: {
       translation: nb_NO,
+    },
+    pl: {
+      translation: pl,
+    },
+    ru: {
+      translation: ru,
+    },
+    uk: {
+      translation: uk,
     },
   },
 });
