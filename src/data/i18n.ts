763617e8947d6bfbd20ba70d110cf4ffd2e9466b
ToastHawaii <mar.pel@protonmail.com>
2023-12-18 12:43:06 +0100

    Add support for pt and merge fr translation

--- src/data/i18n.ts
+++ src/data/i18n.ts
@@ -8,6 +8,7 @@ import * as it from "./locales/it.json";
 import * as ja from "./locales/ja.json";
 import * as nb_NO from "./locales/nb_NO.json";
 import * as pl from "./locales/pl.json";
+import * as pt from "./locales/pt.json";
 import * as ru from "./locales/ru.json";
 import * as uk from "./locales/uk.json";
 
@@ -43,6 +44,9 @@ i18next.init({
     },
     pl: {
       translation: pl,
+    },
+    pt: {
+      translation: pt,
     },
     ru: {
       translation: ru,
