44464d72a9d03e2be85aeb7016cd07cd9f18991d
ToastHawaii <mar.pel@protonmail.com>
2023-12-22 20:22:23 +0100

    Integrate new translations

--- src/data/i18n.ts
+++ src/data/i18n.ts
@@ -5,13 +5,17 @@ import * as cs from "./locales/cs.json";
 import * as de from "./locales/de.json";
 import * as es from "./locales/es.json";
 import * as fr from "./locales/fr.json";
+import * as id from "./locales/id.json";
 import * as it from "./locales/it.json";
 import * as ja from "./locales/ja.json";
+import * as ko from "./locales/ko.json";
 import * as nb_NO from "./locales/nb_NO.json";
 import * as pl from "./locales/pl.json";
 import * as pt from "./locales/pt.json";
 import * as ru from "./locales/ru.json";
 import * as uk from "./locales/uk.json";
+import * as zh_Hant from "./locales/zh_Hant.json";
+import * as zh_Hans from "./locales/zh_Hans.json";
 
 i18next.use(LanguageDetector).init({
   detection: {
@@ -19,41 +23,21 @@ i18next.use(LanguageDetector).init({
   },
   fallbackLng: "en",
   resources: {
-    en: {
-      translation: en,
-    },
-    cs: {
-      translation: cs,
-    },
-    de: {
-      translation: de,
-    },
-    es: {
-      translation: es,
-    },
-    fr: {
-      translation: fr,
-    },
-    it: {
-      translation: it,
-    },
-    ja: {
-      translation: ja,
-    },
-    no: {
-      translation: nb_NO,
-    },
-    pl: {
-      translation: pl,
-    },
-    pt: {
-      translation: pt,
-    },
-    ru: {
-      translation: ru,
-    },
-    uk: {
-      translation: uk,
-    },
+    en: { translation: en },
+    cs: { translation: cs },
+    de: { translation: de },
+    es: { translation: es },
+    fr: { translation: fr },
+    id: { translation: id },
+    it: { translation: it },
+    ja: { translation: ja },
+    ko: { translation: ko },
+    no: { translation: nb_NO },
+    pl: { translation: pl },
+    pt: { translation: pt },
+    ru: { translation: ru },
+    uk: { translation: uk },
+    zh: { translation: zh_Hant },
+    "zh-Hans": { translation: zh_Hans },
   },
 });
