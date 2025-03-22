0a5b3a8f8b04f186626f7438098f1c91d333554b
ToastHawaii <mar.pel@protonmail.com>
2025-03-22 19:57:06 +0100

    Improve performance

--- src/utilities/prepareArrayForSelect.ts
+++ src/utilities/prepareArrayForSelect.ts
@@ -1,29 +1,20 @@
-import { sortBy } from "lodash";
+import { chain } from "lodash";
 import { equalsIgnoreCase } from "./string";
 
 export function prepareArrayForSelect(names: string[], selected: string[]) {
-  names = sortBy(names, (n) => n.toUpperCase());
-
-  const nameCounts: { name: string; count: number }[] = [];
-  for (const name of names) {
-    const nameCount = nameCounts.find((nc) => equalsIgnoreCase(nc.name, name));
-
-    if (nameCount) {
-      nameCount.count++;
-    } else {
-      nameCounts.push({ name, count: 1 });
-    }
-  }
-
-  return nameCounts.map((t) => {
-    if (selected.filter((s) => equalsIgnoreCase(t.name, s)).length > 0) {
-      return { value: t.name, text: t.name, html: t.name, selected: true };
-    } else {
-      return {
-        value: t.name,
-        text: t.name,
-        html: `<span class="select-option">(${t.count})</span>${t.name}`,
-      };
-    }
-  });
+  return chain(names)
+    .groupBy((n) => n.toUpperCase())
+    .map((n) => ({ name: n[0], count: n.length }))
+    .map((t) => {
+      if (selected.filter((s) => equalsIgnoreCase(t.name, s)).length > 0) {
+        return { value: t.name, text: t.name, html: t.name, selected: true };
+      } else {
+        return {
+          value: t.name,
+          text: t.name,
+          html: `<span class="select-option">(${t.count})</span>${t.name}`,
+        };
+      }
+    })
+    .value();
 }
