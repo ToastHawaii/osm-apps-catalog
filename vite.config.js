d276892f5ea1abca851ebcc468f3c547fb5f9a6e
ToastHawaii <mar.pel@protonmail.com>
2026-01-17 17:17:42 +0100

    build: fix file structure for best practies for vite and spa

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
-    root: "./src",
     build: {
-      outDir: "../docs",
+      outDir: "./docs",
     },
     plugins: [react(), tailwindcss()],
     resolve: {
