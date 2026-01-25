f7d34de76dd3098753753d193b794a33752ce2c0
ToastHawaii <mar.pel@protonmail.com>
2026-01-25 16:31:24 +0100

    fix: image and other sources have to be in order

--- actions/lib/utilities/mergeApps.ts
+++ actions/lib/utilities/mergeApps.ts
@@ -18,17 +18,17 @@ export function mergeApps(
   app.unmaintained = mergeBoolean(app.unmaintained, obj.unmaintained);
 
   app.description = app.description || obj.description;
-  app.images = mergeValues(app.images, obj.images);
-  app.logos = mergeValues(app.logos, obj.logos);
+  app.images = mergeValues(app.images, obj.images, { sort: false });
+  app.logos = mergeValues(app.logos, obj.logos, { sort: false });
   app.imageWiki = app.imageWiki || obj.imageWiki;
-  app.commons = mergeValues(app.commons, obj.commons);
-  app.videos = mergeValues(app.videos, obj.videos);
+  app.commons = mergeValues(app.commons, obj.commons, { sort: false });
+  app.videos = mergeValues(app.videos, obj.videos, { sort: false });
 
   app.website = app.website || obj.website;
 
   app.documentation = app.documentation || obj.documentation;
 
-  app.coverage = mergeValues(app.coverage, obj.coverage);
+  app.coverage = mergeValues(app.coverage, obj.coverage, { sort: false });
 
   app.source = mergeAppSources(app.source, obj.source);
 
@@ -38,22 +38,20 @@ export function mergeApps(
   app.libre = mergeBoolean(app.libre, obj.libre);
   app.price = app.price || obj.price;
 
-  app.license = mergeValues(app.license, obj.license);
+  app.license = mergeValues(app.license, obj.license, { sort: true });
 
   app.sourceCode = app.sourceCode || obj.sourceCode;
 
   if (!options.onlyAddLanguageIfEmpty || app.languages.length === 0) {
-    app.languages = mergeValues(app.languages, obj.languages);
+    app.languages = mergeValues(app.languages, obj.languages, { sort: true });
   }
 
   app.languagesUrl = app.languagesUrl || obj.languagesUrl;
 
-  app.genre = mergeValues(app.genre, obj.genre);
-  app.topics = mergeValues(app.topics, obj.topics);
+  app.genre = mergeValues(app.genre, obj.genre, { sort: true });
+  app.topics = mergeValues(app.topics, obj.topics, { sort: true });
 
-  app.platform = mergeValues(app.platform, obj.platform);
-
-  app.coverage = mergeValues(app.coverage, obj.coverage);
+  app.platform = mergeValues(app.platform, obj.platform, { sort: true });
 
   app.install = merge(app.install, obj.install);
 
@@ -81,8 +79,16 @@ function mergeBoolean(b1: boolean | undefined, b2: boolean | undefined) {
   return b2;
 }
 
-function mergeValues(v1: string[] | undefined, v2: string[] | undefined) {
-  return uniqBy([...(v1 || []), ...(v2 || [])], (v) => v.toUpperCase()).sort();
+function mergeValues(
+  v1: string[] | undefined,
+  v2: string[] | undefined,
+  options: { sort: boolean },
+): string[] {
+  const merged = uniqBy([...(v1 || []), ...(v2 || [])], (v) => v.toUpperCase());
+  if (!options?.sort) {
+    return merged;
+  }
+  return merged.sort();
 }
 
 /**
@@ -99,7 +105,7 @@ function mergeFeatures<T extends Record<string, string | string[]>>(
   return mergeWith({}, o1, o2, (objValue, srcValue) => {
     if (Array.isArray(objValue) && Array.isArray(srcValue)) {
       // Concatenate and remove duplicates
-      return mergeValues(objValue, srcValue);
+      return mergeValues(objValue, srcValue, { sort: false });
     }
     // Default merging otherwise
     return undefined;
--- dist/actions/collect-osm-apps-translations/index.js
+++ dist/actions/collect-osm-apps-translations/index.js
@@ -72895,29 +72895,28 @@ function mergeApps(app, obj, options) {
     }
     app.unmaintained = mergeBoolean(app.unmaintained, obj.unmaintained);
     app.description = app.description || obj.description;
-    app.images = mergeValues(app.images, obj.images);
-    app.logos = mergeValues(app.logos, obj.logos);
+    app.images = mergeValues(app.images, obj.images, { sort: false });
+    app.logos = mergeValues(app.logos, obj.logos, { sort: false });
     app.imageWiki = app.imageWiki || obj.imageWiki;
-    app.commons = mergeValues(app.commons, obj.commons);
-    app.videos = mergeValues(app.videos, obj.videos);
+    app.commons = mergeValues(app.commons, obj.commons, { sort: false });
+    app.videos = mergeValues(app.videos, obj.videos, { sort: false });
     app.website = app.website || obj.website;
     app.documentation = app.documentation || obj.documentation;
-    app.coverage = mergeValues(app.coverage, obj.coverage);
+    app.coverage = mergeValues(app.coverage, obj.coverage, { sort: false });
     app.source = mergeAppSources(app.source, obj.source);
     app.author = app.author || obj.author;
     app.gratis = mergeBoolean(app.gratis, obj.gratis);
     app.libre = mergeBoolean(app.libre, obj.libre);
     app.price = app.price || obj.price;
-    app.license = mergeValues(app.license, obj.license);
+    app.license = mergeValues(app.license, obj.license, { sort: true });
     app.sourceCode = app.sourceCode || obj.sourceCode;
     if (!options.onlyAddLanguageIfEmpty || app.languages.length === 0) {
-        app.languages = mergeValues(app.languages, obj.languages);
+        app.languages = mergeValues(app.languages, obj.languages, { sort: true });
     }
     app.languagesUrl = app.languagesUrl || obj.languagesUrl;
-    app.genre = mergeValues(app.genre, obj.genre);
-    app.topics = mergeValues(app.topics, obj.topics);
-    app.platform = mergeValues(app.platform, obj.platform);
-    app.coverage = mergeValues(app.coverage, obj.coverage);
+    app.genre = mergeValues(app.genre, obj.genre, { sort: true });
+    app.topics = mergeValues(app.topics, obj.topics, { sort: true });
+    app.platform = mergeValues(app.platform, obj.platform, { sort: true });
     app.install = (0,lodash.merge)(app.install, obj.install);
     app.map = mergeFeatures(app.map, obj.map);
     app.routing = mergeFeatures(app.routing, obj.routing);
@@ -72937,8 +72936,12 @@ function mergeBoolean(b1, b2) {
         return b1;
     return b2;
 }
-function mergeValues(v1, v2) {
-    return (0,lodash.uniqBy)([...(v1 || []), ...(v2 || [])], (v) => v.toUpperCase()).sort();
+function mergeValues(v1, v2, options) {
+    const merged = (0,lodash.uniqBy)([...(v1 || []), ...(v2 || [])], (v) => v.toUpperCase());
+    if (!options?.sort) {
+        return merged;
+    }
+    return merged.sort();
 }
 /**
  * Merges two objects with string array values, removing duplicates.
@@ -72953,7 +72956,7 @@ function mergeFeatures(o1, o2) {
     return (0,lodash.mergeWith)({}, o1, o2, (objValue, srcValue) => {
         if (Array.isArray(objValue) && Array.isArray(srcValue)) {
             // Concatenate and remove duplicates
-            return mergeValues(objValue, srcValue);
+            return mergeValues(objValue, srcValue, { sort: false });
         }
         // Default merging otherwise
         return undefined;
