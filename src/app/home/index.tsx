5c059e57a1473e078740f69569049eb0637529be
ToastHawaii <mar.pel@protonmail.com>
2026-01-30 14:32:11 +0100

    fix: hide empty categories

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -80,56 +80,58 @@ export function Home({ apps }: { apps: App[] }) {
 
         <div id="list">
           <LazyLoadImages>
-            {categories.map((category, index) => (
-              <React.Fragment key={index}>
-                <div className="grid content-end px-8 pt-3 md:px-18">
-                  <h2 className="text-left text-2xl font-semibold">
-                    {category.name()}
-                  </h2>
-                  <a
-                    className="col-start-2 self-end justify-self-end"
-                    href={`?view=explore&category=${category.id}`}
+            {categories
+              .filter((category) => category.apps.length > 0)
+              .map((category) => (
+                <React.Fragment key={category.id}>
+                  <div className="grid content-end px-8 pt-3 md:px-18">
+                    <h2 className="text-left text-2xl font-semibold">
+                      {category.name()}
+                    </h2>
+                    <a
+                      className="col-start-2 self-end justify-self-end"
+                      href={`?view=explore&category=${category.id}`}
+                    >
+                      Alle anzeigen
+                    </a>
+                  </div>
+                  <Carousel
+                    opts={{
+                      align: "start",
+                    }}
+                    className="w-full px-6 md:px-16"
                   >
-                    Alle anzeigen
-                  </a>
-                </div>
-                <Carousel
-                  opts={{
-                    align: "start",
-                  }}
-                  className="w-full px-6 md:px-16"
-                >
-                  <CarouselContent role="list">
-                    {category.apps.map((app) => (
-                      <CarouselItem
-                        key={app.id}
-                        className="basis-1/1 sm:basis-1/1 md:basis-1/2 lg:basis-1/3"
-                      >
-                        <div className="p-2">
-                          <Item variant="outline" asChild role="listitem">
-                            <a href={`?view=app&app=${app.id}`}>
-                              <ItemMedia variant="icon" className="size-15">
-                                <Logo app={app} />
-                              </ItemMedia>
-                              <ItemContent>
-                                <ItemTitle className="line-clamp-1 wrap-anywhere">
-                                  {app.name}
-                                </ItemTitle>
-                                <ItemDescription className="line-clamp-2 h-10 wrap-anywhere">
-                                  {plainText(app.description)}
-                                </ItemDescription>
-                              </ItemContent>
-                            </a>
-                          </Item>
-                        </div>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious className="left-8" />
-                  <CarouselNext className="right-8" />
-                </Carousel>
-              </React.Fragment>
-            ))}
+                    <CarouselContent role="list">
+                      {category.apps.map((app) => (
+                        <CarouselItem
+                          key={app.id}
+                          className="basis-1/1 sm:basis-1/1 md:basis-1/2 lg:basis-1/3"
+                        >
+                          <div className="p-2">
+                            <Item variant="outline" asChild role="listitem">
+                              <a href={`?view=app&app=${app.id}`}>
+                                <ItemMedia variant="icon" className="size-15">
+                                  <Logo app={app} />
+                                </ItemMedia>
+                                <ItemContent>
+                                  <ItemTitle className="line-clamp-1 wrap-anywhere">
+                                    {app.name}
+                                  </ItemTitle>
+                                  <ItemDescription className="line-clamp-2 h-10 wrap-anywhere">
+                                    {plainText(app.description)}
+                                  </ItemDescription>
+                                </ItemContent>
+                              </a>
+                            </Item>
+                          </div>
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious className="left-8" />
+                    <CarouselNext className="right-8" />
+                  </Carousel>
+                </React.Fragment>
+              ))}
           </LazyLoadImages>
         </div>
       </main>
