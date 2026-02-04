166fa134aab5e4279513786c70d93d7c079bf92b
ToastHawaii <mar.pel@protonmail.com>
2026-02-04 10:50:45 +0100

    feat: make header sticky

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,17 @@
+import { Button } from "@components/ui/button";
 import { Separator } from "@components/ui/separator";
 import React from "react";
 import { Link } from "react-router";
 
 export function Header() {
   return (
-    <>
-      <div className="px-8 py-6">
-        <h2 className="text-left font-semibold">
+    <div className="sticky top-0 z-10 bg-white">
+      <div className="flex px-8 py-4">
+        <Button asChild variant="ghost" className="font-semibold">
           <Link to="/">OSM App Catalog</Link>
-        </h2>
+        </Button>
       </div>
       <Separator />
-    </>
+    </div>
   );
 }
