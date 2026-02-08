b52a94b929ed8ddfe62bb9790295c8f8720ac4ea
ToastHawaii <mar.pel@protonmail.com>
2026-02-08 18:44:16 +0100

    feat: support ScrollRestoration

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,7 +1,65 @@
 import { Header } from "@components/layout/header";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { Outlet } from "react-router";
+import {
+  Outlet,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from "react-router";
+
+export function ScrollRestoration() {
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    const scrollContainer = document.getElementById("content");
+
+    if (!scrollContainer) {
+      return;
+    }
+    // when ever view=... changes in the url, scroll to top or restore last scroll position
+    scrollContainer.scrollTo({
+      top: location.state?.top || 0,
+      left: location.state?.left || 0,
+      behavior: "instant",
+    });
+  }, [searchParams.get("view")]);
+
+  useLayoutEffect(() => {
+    const scrollContainer = document.getElementById("content");
+
+    if (!scrollContainer) {
+      return;
+    }
+
+    // after scroll: save current position for browser history changes
+    const handleScroll = () => {
+      navigate(
+        {
+          pathname: location.pathname,
+          search: location.search,
+          hash: location.hash,
+        },
+        {
+          replace: true,
+          state: {
+            top: scrollContainer.scrollTop,
+            left: scrollContainer.scrollLeft,
+          },
+        },
+      );
+    };
+    scrollContainer.addEventListener("scroll", handleScroll);
+
+    return () => {
+      scrollContainer.removeEventListener("scroll", handleScroll);
+    };
+  }, [searchParams]);
+
+  return null;
+}
 
 export default function Layout() {
   const { i18n } = useTranslation();
@@ -9,9 +67,12 @@ export default function Layout() {
   document.documentElement.lang = i18n.resolvedLanguage || "en";
 
   return (
-    <div id="content">
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <ScrollRestoration />
+      <div id="content">
+        <Header />
+        <Outlet />
+      </div>
+    </>
   );
 }
