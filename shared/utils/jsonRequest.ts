8f13071306c721903ef6051f93529206157066e5
ToastHawaii <mar.pel@protonmail.com>
2026-05-14 08:20:31 +0200

    fix: use githubusercontent directly on dev

--- shared/utils/jsonRequest.ts
+++ shared/utils/jsonRequest.ts
@@ -9,10 +9,15 @@ export async function getJson(
 ): Promise<any> {
   if (isDevelopment) {
     const response = await fetch(
-      "https://corsproxy.io/?" +
-        encodeURIComponent(`${url}?${utilQsString(params)}`) +
-        // change to avoid caching during testing
-        "%262026-04-15",
+      url.startsWith("https://osm-apps.org/")
+        ? url.replace(
+            "https://osm-apps.org/",
+            "https://raw.githubusercontent.com/ToastHawaii/osm-apps-catalog/refs/heads/main/docs/",
+          )
+        : "https://corsproxy.io/?" +
+            encodeURIComponent(`${url}?${utilQsString(params)}`) +
+            // change to avoid caching during testing
+            "%262026-04-15",
     );
 
     return await response.json();
