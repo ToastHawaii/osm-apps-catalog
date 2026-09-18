ebfc49750b0b7574baca5cd74a44dc14018c1d9a
ToastHawaii <mar.pel@protonmail.com>
2026-09-18 20:27:52 +0200

    enhance: add retry to getStats

--- actions/lib/getStats.ts
+++ actions/lib/getStats.ts
@@ -1,32 +1,17 @@
 import * as core from "@actions/core";
+import { delay } from "@shared/utils/delay";
 import { getJson } from "@shared/utils/jsonRequest";
 
 export async function getStats() {
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
+  let { hits } = await requestStats();
 
-  const formatDate = (date: Date) => date.toISOString().split("T")[0];
-  const currentDate = formatDate(yesterday);
-
-  const sevenDaysAgo = new Date(yesterday);
-  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 8);
-  const currentMinus7 = formatDate(sevenDaysAgo);
+  if (!hits) {
+    await delay(5000);
+    hits = (await requestStats()).hits;
+  }
 
   return (
-    (
-      (await getJson(
-        "https://osm-apps.goatcounter.com/api/v0/stats/hits",
-        {
-          start: currentMinus7,
-          end: currentDate,
-          group: "day",
-          limit: 50,
-        },
-        {
-          Authorization: `Bearer ${core.getInput("goatcounterToken")}`,
-        },
-      )) as { hits: { count: number; path: string }[] }
-    ).hits
+    hits
       // get all stats to apps
       .filter((hit) => /app\/\d+$/.test(hit.path))
       .map((hit) => ({
@@ -34,4 +19,28 @@ export async function getStats() {
         count: hit.count,
       }))
   );
+}
+
+async function requestStats() {
+  const today = new Date();
+
+  const formatDate = (date: Date) => date.toISOString().split("T")[0];
+  const currentDate = formatDate(today);
+
+  const sevenDaysAgo = new Date(today);
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+  const currentMinus7 = formatDate(sevenDaysAgo);
+
+  return (await getJson(
+    "https://osm-apps.goatcounter.com/api/v0/stats/hits",
+    {
+      start: currentMinus7,
+      end: currentDate,
+      group: "day",
+      limit: 50,
+    },
+    {
+      Authorization: `Bearer ${core.getInput("goatcounterToken")}`,
+    },
+  )) as { hits: { count: number; path: string }[] };
 }
